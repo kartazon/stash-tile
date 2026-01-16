9afacc82e45d6966b3cab7c44225925bf96f42f6
kartazon <44315828+kartazon@users.noreply.github.com>
2026-01-16 08:56:22 +0000

    Update ipcheck.js

--- tiles/ipcheck.js
+++ tiles/ipcheck.js
@@ -1,35 +1,28 @@
 /* jshint esversion: 6 */
 let url = "http://ip-api.com/json/?lang=en";
-const align = (text, width = 10) => text.padEnd(width, ' ');
 
 $httpClient.get(url, function(error, response, data) {
-    if (error || !data) {
-        $done({title: "Ошибка", content: "Нет ответа от сервера"});
+    if (error) {
+        $done({title: "Ошибка", content: "Не удалось получить данные"});
         return;
     }
 
-    try {
-        let jsonData = JSON.parse(data);
-        let country = jsonData.country || "Unknown";
-        let emoji = getFlagEmoji(jsonData.countryCode);
-        let city = jsonData.city || "";
-        let timezone = jsonData.timezone || "";
-        let isp = jsonData.isp || "";
-        let ip = jsonData.query || "";
+    let jsonData = JSON.parse(data);
+    let country = jsonData.country;
+    let emoji = getFlagEmoji(jsonData.countryCode);
+    let city = jsonData.city;
+    let timezone = jsonData.timezone;
+    let isp = jsonData.isp;
+    let ip = jsonData.query;
 
-        let body = {
-            title: "IP info",
-            content: `${align("IP:")}${ip}\n` +
-                     `${align("ORG:")}${isp}\n` +
-                     `${align("REG:")}${emoji} ${country}\n` +
-                     `${align("TMZ:")}${timezone}`,
-            icon: "globe.asia.australia.fill",
-            "icon-color": "#0C9DFA"
-        };
-        $done(body);
-    } catch (e) {
-        $done({title: "Ошибка", content: "Ошибка парсинга данных"});
-    }
+    let body = {
+        title: "IP info",
+		content: `${ip}\t |\t IP\n${isp}\t |\t ORG\n${emoji} ${country} - ${city}\t |\t REG\n${timezone}\t |\t TMZ`,
+        icon: "globe.asia.australia.fill",
+        backgroundColor: '#0C9DFA'
+    };
+
+    $done(body);
 });
 
 function getFlagEmoji(countryCode) {
