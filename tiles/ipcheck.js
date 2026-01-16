078aa5fd44a75906b984b43ab64ea4062d7adeac
kartazon <44315828+kartazon@users.noreply.github.com>
2026-01-16 07:13:41 +0000

    Update ipcheck.js

--- tiles/ipcheck.js
+++ tiles/ipcheck.js
@@ -7,16 +7,26 @@ $httpClient.get('http://ip-api.com/json', function(error, response, data) {
 
     let jsonData = JSON.parse(data);
     let country = jsonData.country;
-    let emoji = getFlagEmoji(jsonData.countryCode);
+    let emoji = getFlagEmoji(jsonData.countryCode); // Вызываем функцию получения флага
     let city = jsonData.city;
     let isp = jsonData.isp;
     let ip = jsonData.query;
-    
+
     let body = {
-        content: `${ip} | IP\n${isp} | ORG\n${emoji}${country} - ${city} | REG`,
+        // Исправлено: добавлена $ перед {emoji}
+        content: `${ip} | IP\n${isp} | ORG\n${emoji} ${country} - ${city} | REG`,
         icon: "globe.asia.australia.fill",
+        // "icon-color": "#0C9DFA"
         backgroundColor: '#0C9DFA'
     };
 
     $done(body);
 });
+
+
+function getFlagEmoji(countryCode) {
+    if (!countryCode) return "🏳️";
+    return countryCode
+        .toUpperCase()
+        .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt(0)));
+}
